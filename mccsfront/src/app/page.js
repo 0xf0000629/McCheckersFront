428f30c57cd61c0d14c4e4552431403fd624bc9c
0xf0000629 <xfutur85@gmail.com>
2024-11-30 16:42:46 +0300

    modified:   mccsfront/src/app/page.js

--- mccsfront/src/app/page.js
+++ mccsfront/src/app/page.js
@@ -1,94 +1,121 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [login, setlogin] = useState('');
+  const [password, setpassword] = useState('');
+
+  const [thesurname, setsurname] = useState('');
+  const [thename, setname] = useState('');
+  const [thephone, setphone] = useState('');
+
+
+  const [activeForm, setActiveForm] = useState('login');
+
+  const handleLog = (e) => {setlogin(e.target.value);};
+  const handlePass = (e) => {setpassword(e.target.value);};
+
+  const handleSN = (e) => {setsurname(e.target.value);};
+  const handleN = (e) => {setname(e.target.value);};
+  const handlePH = (e) => {setphone(e.target.value);};
+
+
+  const handleSubmit = (e) => { e.preventDefault(); alert(`"Login:": ${login}, "Password:": ${password}`); };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
         <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+        <h1>oughhhhh register me</h1>
+      
+        {/* Buttons to Switch Forms */}
+        <div style={{ marginBottom: '20px' }}>
+          <button
+            onClick={() => {if (activeForm === 'login') setActiveForm('register'); else setActiveForm('login');}}
+            style={{
+              padding: '10px',
+              marginRight: '10px',
+              background: activeForm === 'login' ? 'lightblue' : 'white',
+            }}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            SWITCH
+          </button>
+        </div>
+
+
+        {activeForm === 'login' && (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Username..."
+              value={login}
+              onChange={handleLog}
+              style={{ padding: '8px', marginRight: '10px' }}
+            /><br/>
+            <input
+              type="text"
+              placeholder="Password..."
+              value={password}
+              onChange={handlePass}
+              style={{ padding: '8px', marginRight: '10px' }}
+            /><br/>
+            <button type="submit" style={{ padding: '8px' }}>
+              get me in
+            </button>
+          </form>
+        )}
+
+        {activeForm === 'register' && (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Surname..."
+              value={thesurname}
+              onChange={handleSN}
+              style={{ padding: '8px', marginRight: '10px' }}
+            /><br/>
+            <input
+              type="text"
+              placeholder="Name..."
+              value={thename}
+              onChange={handleN}
+              style={{ padding: '8px', marginRight: '10px' }}
+            /><br/>
+            <input
+              type="text"
+              placeholder="Phone number..."
+              value={thephone}
+              onChange={handlePH}
+              style={{ padding: '8px', marginRight: '10px' }}
+            /><br/>
+            <input
+              type="text"
+              placeholder="Username..."
+              value={login}
+              onChange={handleLog}
+              style={{ padding: '8px', marginRight: '10px' }}
+            /><br/>
+            <input
+              type="text"
+              placeholder="Password..."
+              value={password}
+              onChange={handlePass}
+              style={{ padding: '8px', marginRight: '10px' }}
+            /><br/>
+            <button type="submit" style={{ padding: '8px' }}>
+              register me
+            </button>
+          </form>
+        )}
+
         </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        
       </footer>
     </div>
   );
