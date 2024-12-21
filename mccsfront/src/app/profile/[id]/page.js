8482d2b3a421e0979746f33f3591b061525ce591
0xf0000629 <xfutur85@gmail.com>
2024-12-21 05:30:27 +0300

    modified:   mccsfront/src/app/leaderboard/page.js 	modified:   mccsfront/src/app/profile/[id]/page.js

--- mccsfront/src/app/profile/[id]/page.js
+++ mccsfront/src/app/profile/[id]/page.js
@@ -107,7 +107,7 @@ export default function Profile() {
   const fetchMe = async () => {
     if (localStorage.getItem("me") != undefined) {
       let loadme = JSON.parse(localStorage.getItem("me"));
-      setMe(me);
+      setMe(loadme);
     }
   };
 
